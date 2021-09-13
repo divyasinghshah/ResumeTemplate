c16def314403fc121a1065a08f0a0e6d3b4ed844
divyasinghshah <divyasinghshah@gmail.com>
2021-09-13 18:36:53 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,109 @@
+function about(){
+
+	targetPos=300;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+
+
+function skills(){
+
+	targetPos=900;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+
+function experience(){
+
+	targetPos=1500;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+
+function education(){
+
+	targetPos=2532;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+
+function portfolio(){
+
+	targetPos=3600;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+function contact(){
+
+	targetPos=5532;
+	currentPos=0;
+
+	let scrollInterval=setInterval(function(){
+		if(currentPos>=targetPos)
+		{
+			clearInterval(scrollInterval);
+			return;
+		}
+
+		currentPos+=50;
+		window.scrollBy(0,50);
+
+	},15);
+}
+
+
